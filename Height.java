757f1afeda7aaa3cd2ea1ad3417f106932f4a691
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-21 12:49:10 -0500

    format classes Height Player and Project1

---
+++ Height.java
@@ -0,0 +1,22 @@
+/**
+ * Height
+ */
+public class Height {
+
+    private int feet;
+    private int inches;
+
+    public Height(int feet, int inches) {
+        
+        this.feet = feet;
+        this.inches = inches;
+    }
+
+    public int toInches() {
+
+    }
+
+    public String toString() {
+        
+    }
+}
---
+++ Player.java
@@ -0,0 +1,29 @@
+class Player {
+
+    private String name;
+    private int age;
+    private Height height;
+
+    public Player(String name, int age, Height height) {
+        
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public Height getHeight() {
+        return this.height;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String toString() {
+        return " ";
+    }
+}
